7539a3f195703afc96a1d5f217ab069b9801c262
Dave <Dave.Troupe@gmail.com>
2017-02-10 10:31:41 -0500

    Clean up code and add quit button

--- src/GUI_Room_Escape.java
+++ src/GUI_Room_Escape.java
@@ -7,7 +7,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,25 +22,34 @@ public class GUI_Room_Escape extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
+
+        window = primaryStage; // main screen
 
         Label question1 = new Label("You are in a spooky classroom with lots of students\n " +
                 "do you teach or run for the hills?");
         Button button1 = new Button("Teach");
         Button button2 = new Button("Run For It");
+        Button quitButton = new Button("Quit");
 
         button1.setOnAction(e -> window.setScene(fail));
         button2.setOnAction(e -> window.setScene(scene2));
 
         // Center everything
         VBox initialScreen = new VBox(10);
-        initialScreen.getChildren().addAll(question1, button1, button2);
-        StackPane view1 = new StackPane();
-        view1.getChildren().add(initialScreen);
-        StackPane.setAlignment(initialScreen, Pos.CENTER);
+        initialScreen.getChildren().addAll(question1, button1, button2, quitButton);
+        //initialScreen.setAlignment(Pos.CENTER);
 
-        scene1 = new Scene(view1, 400, 400);
-        view1.setStyle("-fx-background-color: #F0591E;");
+
+        quitButton.setOnAction(e -> {
+            boolean quit = ConfirmBox.display("Quit", "Are you sure you want to quit?");
+            if (quit) {
+                window.close();
+            }
+        });
+
+
+        scene1 = new Scene(initialScreen, 400, 400);
+        initialScreen.setStyle("-fx-background-color: #F0591E;");
 
         // CREATE FAIL SCENE
         Label failed = new Label("You Lose");
@@ -50,11 +58,10 @@ public class GUI_Room_Escape extends Application {
 
         VBox failScreen = new VBox(5);
         failScreen.getChildren().addAll(failed, button3);
-        StackPane failedView = new StackPane();
-        failedView.getChildren().add(failScreen);
-        StackPane.setAlignment(failScreen, Pos.CENTER);
-        fail = new Scene(failedView, 400, 400);
-        failedView.setStyle("-fx-background-color: chartreuse");
+        failScreen.setAlignment(Pos.CENTER);
+        fail = new Scene(failScreen, 400, 400);
+        failScreen.setStyle("-fx-background-color: chartreuse");
+
 
         // CREATE SCENE2
         Label question2 = new Label("Are you smart?");
@@ -65,11 +72,9 @@ public class GUI_Room_Escape extends Application {
 
         VBox secondScene = new VBox(5);
         secondScene.getChildren().addAll(question2, button4, button5);
-        StackPane secondView = new StackPane();
-        secondView.getChildren().add(secondScene);
-        StackPane.setAlignment(secondScene, Pos.CENTER);
-        scene2 = new Scene(secondView, 400, 400);
-        secondView.setStyle("-fx-background-color: darksalmon");
+        secondScene.setAlignment(Pos.CENTER);
+        scene2 = new Scene(secondScene, 400, 400);
+        secondScene.setStyle("-fx-background-color: darksalmon");
 
 
         // CREATE SCENE 3
@@ -82,26 +87,20 @@ public class GUI_Room_Escape extends Application {
 
         VBox thirdScene = new VBox(5);
         thirdScene.getChildren().addAll(question3, button6, button7);
-        StackPane thirdView = new StackPane();
-        thirdView.getChildren().add(thirdScene);
-        StackPane.setAlignment(thirdScene, Pos.CENTER);
-        scene3 = new Scene(thirdView, 400, 400);
-        thirdView.setStyle("-fx-background-color: deeppink");
+        thirdScene.setAlignment(Pos.CENTER);
+        scene3 = new Scene(thirdScene, 400, 400);
+        thirdScene.setStyle("-fx-background-color: deeppink");
 
         // CREATE WIN SCENE
         Label youWin = new Label("You WIN!");
         Button button8 = new Button("Play Again");
         button8.setOnAction(e -> window.setScene(scene1));
 
-
         VBox winScreen = new VBox(5);
         winScreen.getChildren().addAll(youWin, button8);
-        StackPane winView = new StackPane();
-        winView.getChildren().add(winScreen);
-        StackPane.setAlignment(winScreen, Pos.CENTER);
-        win = new Scene(winView, 400, 400);
-        winView.setStyle("-fx-background-color: khaki");
-
+        winScreen.setAlignment(Pos.CENTER);
+        win = new Scene(winScreen, 400, 400);
+        winScreen.setStyle("-fx-background-color: khaki");
 
         // how do we know what scene to display by default?
         window.setScene(scene1);
